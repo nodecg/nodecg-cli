ce8e58e62469e2194ec3d86289200dbbfddc5acc
Alex Van Camp <email@alexvan.camp>
2015-01-17 21:28:22 -0600

    add update command

--- bin/nodecg-install.js
+++ bin/nodecg-install.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
-// Nodejs libs
-var fs = require('fs');
-
 // External libs
 var program = require('commander');
 var npa = require('npm-package-arg');
 var git = require('../lib/git');
 var findup = require('findup-sync');
 var path = require('path');
-var npm = require('npm');
+
+// Internal libs
+var installDeps = require('../lib/install-deps');
 
 var nodecgPath = findup('nodecg/');
 if (!nodecgPath) {
@@ -23,8 +22,8 @@ program
     .parse(process.argv);
 
 if (!program.args[0]) {
-    // If no args are supplied, assume the user is intending to update the bundle in the current dir
-    installDeps(process.cwd());
+    // If no args are supplied, assume the user is intending to operate on the bundle in the current dir
+    installDeps(process.cwd(), program.dev);
 } else {
     var parsed = npa(program.args[0]);
     var repoUrl = null;
@@ -52,48 +51,7 @@ if (!program.args[0]) {
         var temp = repoUrl.split('/').pop();
         var bundleName = temp.substr(0, temp.length - 4);
         var bundlePath = path.join(nodecgPath, 'bundles/', bundleName);
-        installDeps(bundlePath);
-    });
-    process.chdir(cwd);
-}
-
-function installDeps(bundlePath) {
-    var manifestPath = path.join(bundlePath, 'nodecg.json');
-    if (!fs.existsSync(manifestPath)) {
-        console.error('nodecg.json not found, are you in a bundle directory?');
-        process.exit(1);
-    }
-
-    var bundle = require(manifestPath);
-    npm.load({ prefix: process.cwd(), loglevel: 'warn' }, function npmLoaded(err) {
-        if (err) {
-            console.errror('Failed to load npm:', err.message);
-            return;
-        }
-
-        // npm.commands.install takes an array of string arguments
-        // we must take the bundle's list of deps and turn them into arg strings
-        var args = [];
-        for (var dep in bundle.dependencies) {
-            if (!bundle.dependencies.hasOwnProperty(dep)) continue;
-            args.push(dep + '@' + bundle.dependencies[dep]);
-        }
-        if (program.dev) {
-            for (dep in bundle.devDependencies) {
-                if (!bundle.devDependencies.hasOwnProperty(dep)) continue;
-                args.push(dep + '@' + bundle.devDependencies[dep]);
-            }
-        }
-
-        if (args.length <= 0) {
-            console.log('No dependencies to install.');
-            return;
-        }
-
-        npm.commands.install(args, function afterNpmInstall(err, data) {
-            if (err) {
-                console.error('Failed to install dependencies:', err.message);
-            }
-        });
+        installDeps(bundlePath, program.dev);
+        process.chdir(cwd);
     });
 }
