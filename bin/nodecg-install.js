06ac910775e0622492560014b724b3382e5f9e32
Alex Van Camp <email@alexvan.camp>
2015-01-17 19:25:27 -0600

    `nodecg install` now works with no args provided if the cwd is a bundle

--- bin/nodecg-install.js
+++ bin/nodecg-install.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-var program = require('commander');
+// Nodejs libs
+var fs = require('fs');
 
 // External libs
+var program = require('commander');
 var npa = require('npm-package-arg');
 var git = require('../lib/git');
 var findup = require('findup-sync');
 var path = require('path');
+var npm = require('npm');
 
 var nodecgPath = findup('nodecg/');
 if (!nodecgPath) {
@@ -20,35 +23,73 @@ program
     .parse(process.argv);
 
 if (!program.args[0]) {
-    console.log('Hay do stuff if no arg is supplied');
-    process.exit(0);
-}
-
-var parsed = npa(program.args[0]);
-var repoUrl = null;
-
-if (parsed.type === 'git') {
-    repoUrl = parsed.spec;
-} else if (parsed.type === 'hosted') {
-    repoUrl = parsed.hosted.httpsUrl;
-} else {
-    console.error('Please enter a valid git url (https) or GitHub username/repo pair.');
-    process.exit(1);
-}
-
-var bundlesPath = path.join(nodecgPath, 'bundles');
-var cwd = process.cwd();
-process.chdir(bundlesPath);
-git.clone(repoUrl, '', function(err) {
-    if (err) {
-        console.error(err.message);
-        process.chdir(cwd);
+    // If no args are supplied, assume the user is intending to update the bundle in the current dir
+    var manifestPath = path.join(process.cwd(), 'nodecg.json');
+    if (!fs.existsSync(manifestPath)) {
+        console.error('nodecg.json not found, are you in a bundle directory?');
         process.exit(1);
     }
-});
+    
+    var bundle = require(manifestPath);
+    npm.load({ prefix: process.cwd(), loglevel: 'warn' }, function npmLoaded(err) {
+        if (err) {
+            console.errror('Failed to load npm:', err.message);
+            return;
+        }
 
-process.chdir(cwd);
+        // npm.commands.install takes an array of string arguments
+        // we must take the bundle's list of deps and turn them into arg strings
+        var args = [];
+        for (var dep in bundle.dependencies) {
+            if (!bundle.dependencies.hasOwnProperty(dep)) continue;
+            args.push(dep + '@' + bundle.dependencies[dep]);
+        }
 
-if (program.dev) {
-    // take the contents of nodecg-installdev.js and put them in here
+        if (program.dev) {
+            for (dep in bundle.devDependencies) {
+                if (!bundle.devDependencies.hasOwnProperty(dep)) continue;
+                args.push(dep + '@' + bundle.devDependencies[dep]);
+            }
+        }
+
+        if (args.length <= 0) {
+            console.log('Nothing to install.');
+            return;
+        }
+
+        npm.commands.install(args, function afterNpmInstall(err, data) {
+            if (err) {
+                console.error('Failed to install dependencies:', err.message);
+            }
+        });
+    });
+} else {
+    var parsed = npa(program.args[0]);
+    var repoUrl = null;
+
+    if (parsed.type === 'git') {
+        repoUrl = parsed.spec;
+    } else if (parsed.type === 'hosted') {
+        repoUrl = parsed.hosted.httpsUrl;
+    } else {
+        console.error('Please enter a valid git url (https) or GitHub username/repo pair.');
+        process.exit(1);
+    }
+
+    var bundlesPath = path.join(nodecgPath, 'bundles');
+    var cwd = process.cwd();
+    process.chdir(bundlesPath);
+    git.clone(repoUrl, '', function(err) {
+        if (err) {
+            console.error(err.message);
+            process.chdir(cwd);
+            process.exit(1);
+        }
+    });
+
+    process.chdir(cwd);
+
+    if (program.dev) {
+        // take the contents of nodecg-installdev.js and put them in here
+    }
 }
