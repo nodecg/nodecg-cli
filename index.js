82a55839ed564e07e77df83a8535a7b98e164ba0
Alex Van Camp <email@alexvan.camp>
2016-02-07 12:53:09 -0600

    Actually remove version-compare lib this time, replace with semver

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 process.title = 'nodecg';
 
 var request = require('request');
-var versionCompare = require('./lib/version-compare');
+var semver = require('semver');
 var chalk = require('chalk');
 var program = new (require('commander').Command)('nodecg');
 
@@ -12,7 +12,7 @@ var pjson = require('./package.json');
 // Check for updates
 request('http://registry.npmjs.org/nodecg-cli/latest', function (err, res, body) {
     if (!err && res.statusCode === 200) {
-        if (versionCompare(JSON.parse(body).version, pjson.version) >= 1) {
+        if (semver.gt(JSON.parse(body).version, pjson.version)) {
             console.log(chalk.yellow('?')+' A new update is available for nodecg-cli: ' +
                 chalk.green.bold(JSON.parse(body).version) + chalk.dim(' (current: ' + pjson.version + ')'));
             console.log('  Run ' + chalk.cyan.bold('npm install -g nodecg-cli') + ' to install the latest version');
