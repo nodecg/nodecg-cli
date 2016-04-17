892f23cb2e4a31e6cca2dcd02cef51c3417da31f
Alex Van Camp <email@alexvan.camp>
2016-04-16 20:11:48 -0500

    whoops pt. deux

--- test/commands/4_uninstall.mspec.js
+++ test/commands/4_uninstall.mspec.js
@@ -16,18 +16,18 @@ describe('uninstall command', function () {
 		uninstallCommand = new UninstallCommand(program);
 	});
 
-	it('should delete the bundle\'s folder after prompting for ocnfirmation', function () {
-		this.timeout(25000);
+	it('should delete the bundle\'s folder after prompting for confirmation', function (done) {
+		this.timeout(5000);
 		sinon.stub(inquirer, 'prompt')
 			.resolves({confirmUninstall: true})()
 			.then(function () {
 				assert.equal(fs.existsSync('./bundles/lfg-streamtip'), false);
 				inquirer.prompt.restore();
+				done();
 			});
 	});
 
 	it('should print an error when the target bundle is not installed', function () {
-		this.timeout(5000);
 		sinon.spy(console, 'error');
 		program.runWith('uninstall not-installed');
 		assert.equal('Cannot uninstall %s: bundle is not installed.',
