035e71ef069322986c28518ac4a6be18b6251917
Hoishin <hoishinxii@gmail.com>
2019-01-28 07:24:16 +0900

    fix: correct require path to package.json

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ import chalk from 'chalk';
 import {Command} from 'commander';
 
 const program = new Command('nodecg');
-const pjson = require('./package.json');
+const packageVersion: string = require('../package.json')
 
 // Check for updates
 request('http://registry.npmjs.org/nodecg-cli/latest', (err, res, body) => {
 	if (!err && res.statusCode === 200) {
-		if (semver.gt(JSON.parse(body).version, pjson.version)) {
+		if (semver.gt(JSON.parse(body).version, packageVersion)) {
 			console.log(chalk.yellow('?') + ' A new update is available for nodecg-cli: ' +
-				chalk.green.bold(JSON.parse(body).version) + chalk.dim(' (current: ' + pjson.version + ')'));
+				chalk.green.bold(JSON.parse(body).version) + chalk.dim(' (current: ' + packageVersion + ')'));
 			console.log('  Run ' + chalk.cyan.bold('npm install -g nodecg-cli') + ' to install the latest version');
 		}
 	}
@@ -21,7 +21,7 @@ request('http://registry.npmjs.org/nodecg-cli/latest', (err, res, body) => {
 
 // Initialise CLI
 program
-	.version(require('./package.json').version)
+	.version(packageVersion)
 	.usage('<command> [options]');
 
 // Initialise commands
