01ebd6318c2dfa614ec4eb457ab132e2783b815a
Alex Van Camp <vancamp.alex@gmail.com>
2023-01-14 18:27:05 -0600

    feat: add option to disable source map support

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -5,11 +5,16 @@ import { execSync } from 'child_process';
 export = function (program: Command) {
 	program
 		.command('start')
+		.option('-d, --disable-source-maps', 'Disable source map support')
 		.description('Start NodeCG')
-		.action(() => {
+		.action((options: { disableSourceMaps: boolean }) => {
 			// Check if nodecg is already installed
 			if (util.pathContainsNodeCG(process.cwd())) {
-				execSync('node --enable-source-maps index.js', { stdio: ['pipe', 'pipe', 'pipe'] });
+				if (options.disableSourceMaps) {
+					execSync('node index.js', { stdio: ['pipe', 'pipe', 'pipe'] });
+				} else {
+					execSync('node --enable-source-maps index.js', { stdio: ['pipe', 'pipe', 'pipe'] });
+				}
 			} else {
 				console.warn('No NodeCG installation found in this folder.');
 			}
