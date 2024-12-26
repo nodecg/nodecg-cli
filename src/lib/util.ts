e0ce4c91492c914c170e78d4d49f0634225c3dad
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-12-26 20:02:13 +0100

    fix: remove node-fetch

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import fetch from 'node-fetch';
 import semver from 'semver';
 
 export default {
@@ -85,7 +84,7 @@ export default {
 			throw new Error(`Failed to fetch NodeCG release information from npm, status code ${res.status}`);
 		}
 
-		return res.json();
+		return res.json() as Promise<NPMRelease>;
 	},
 };
 
