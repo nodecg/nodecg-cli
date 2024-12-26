9cabed4e01b290b5d0af09839501adea1b6393b7
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-12-26 18:38:45 +0100

    uh

--- test/commands/schema-types.spec.ts
+++ test/commands/schema-types.spec.ts
@@ -67,7 +67,9 @@ it('should successfully compile the config schema', async () => {
 	const outputPath = './src/types/schemas/configschema.d.ts';
 	expect(fs.existsSync(outputPath)).toBe(true);
 
-	expect(fs.readFileSync(outputPath, 'utf8')).toMatchFileSnapshot('../fixtures/results/schema-types/configschema.d.ts');
+	expect(fs.readFileSync(outputPath, 'utf8')).toMatchFileSnapshot(
+		'../fixtures/results/schema-types/configschema.d.ts',
+	);
 	expect(fs.readFileSync('./src/types/schemas/index.d.ts', 'utf8')).toMatchInlineSnapshot(`
 		"/* eslint-disable */
 		// @ts-ignore
