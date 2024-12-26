39c7a9cbe7b8e594258d268e093a06242cd660d2
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-12-26 18:34:44 +0100

    ok

--- test/commands/schema-types.spec.ts
+++ test/commands/schema-types.spec.ts
@@ -43,13 +43,11 @@ it('should successfully create d.ts files from the replicant schemas and create 
 	const outputPath = './src/types/schemas/example.d.ts';
 	expect(fs.existsSync(outputPath)).toBe(true);
 
-	expect(fs.readFileSync(outputPath, 'utf8')).toBe(
-		fs.readFileSync('../../results/schema-types/example.d.ts', 'utf8'),
-	);
+	expect(fs.readFileSync(outputPath, 'utf8')).toMatchFileSnapshot('../fixtures/results/schema-types/example.d.ts');
 
 	const indexPath = './src/types/schemas/index.d.ts';
 	expect(fs.existsSync(indexPath)).toBe(true);
-	expect(fs.readFileSync(indexPath, 'utf8')).toBe(fs.readFileSync('../../results/schema-types/index.d.ts', 'utf8'));
+	expect(fs.readFileSync(indexPath, 'utf8')).toMatchFileSnapshot('../fixtures/results/schema-types/index.d.ts');
 });
 
 it('should print an error when the target bundle does not have a schemas dir', async () => {
@@ -69,12 +67,13 @@ it('should successfully compile the config schema', async () => {
 	const outputPath = './src/types/schemas/configschema.d.ts';
 	expect(fs.existsSync(outputPath)).toBe(true);
 
-	expect(fs.readFileSync(outputPath, 'utf8')).toBe(
-		fs.readFileSync('../../results/schema-types/configschema.d.ts', 'utf8'),
-	);
-	expect(fs.readFileSync('./src/types/schemas/index.d.ts', 'utf8')).toBe(
-		"/* eslint-disable */\n// @ts-ignore\nexport * from './configschema';\n",
-	);
+	expect(fs.readFileSync(outputPath, 'utf8')).toMatchFileSnapshot('../fixtures/results/schema-types/configschema.d.ts');
+	expect(fs.readFileSync('./src/types/schemas/index.d.ts', 'utf8')).toMatchInlineSnapshot(`
+		"/* eslint-disable */
+		// @ts-ignore
+		export * from './configschema';
+		"
+	`);
 });
 
 async function waitForEvent(emitter: EventEmitter, eventName: string) {
